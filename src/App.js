f1c77d48c38ea65bc3c953de973fa4bb7f7198c3
Jorge Mollericon <jlmollericon@gmail.com>
2022-05-25 14:06:20 -0400

    ✨ implementation of the store for the auth module

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
-import "./App.css";
+import { useSelector } from "react-redux";
+
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 
+import "./App.css";
+
 function App() {
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
+
   return (
     <div className="App">
-      <Auth />
-      {/* <Layout /> */}
+      { !isLoggedIn && <Auth />}
+      { isLoggedIn && <Layout /> }
     </div>
   );
 }
